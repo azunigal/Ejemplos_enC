c3a4b2d4c09d41d63230c71766adc07c0ac3fac7
azunigal <azunigamx@yahoo.com.mx>
2021-08-24 11:17:40 -0500

    cambios

---
+++ main.c
@@ -0,0 +1,15 @@
+/* 
+Comentario 
+nombre del programa
+Autor
+Modificado por
+que hace el programa
+*/
+
+#include <stdio.h>
+
+int main() {
+    int a;
+    printf("hola mundo 2");
+    return 0;
+}
